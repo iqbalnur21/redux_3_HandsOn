ab92d14cd3ad45cd07185d41f08071a3bea8bff5
iqbal21 <m.iqbalnur21.min@gmail.com>
2024-03-17 00:33:00 +0800

    update

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,40 +1,75 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 import classes from "./Counter.module.css";
-import counterReducer from "../features/counterSlice";
+import { useDispatch } from "react-redux";
+
+const initialState = {
+  count: 0,
+  step: 1,
+  show: true,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "inc10":
+      return { ...state, count: state.count + 10 };
+    case "toogle":
+      return { ...state, show: !state.show };
+    default:
+      throw new Error("Unknown action");
+  }
+};
 
 const Counter = () => {
-  const [state, dispatch] = useReducer(counterReducer, initialState);
-  const { count, step } = state;
-  const counter = 0;
-  const show = true;
+  // const dispatch = useDispatch();
+  // const [count, setCount] = useState(0);
+  // const [show, setShow] = useState(true);
 
-  const inc = () => {
-    dispatch({ type: "inc" });
-  };
+  // const inc = () => {
+  //   dispatch(inc());
+  // };
 
-  const inc10 = () => {
-    dispatch({ type: "inc10" });
-  };
+  // const dec = () => {
+  //   dispatch(dec());
+  // };
+  // const inc10 = () => {
+  //   dispatch(inc10());
+  // };
+  // const toogle = () => {
+  //   dispatch(toogle());
+  //   setShow(!show);
+  // };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, show } = state;
 
   const dec = () => {
     dispatch({ type: "dec" });
   };
 
-  const defineCount = (e) => {
-    dispatch({ type: "setCount", payload: Number(e.target.value) });
+  const inc = () => {
+    dispatch({ type: "inc" });
   };
+  const inc10 = () => {
+    dispatch({ type: "inc10" });
+  };
+  const toogle = () => {
+    dispatch({ type: "toogle" });
+  };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      {show && (
-        <input value={count} className={classes.value} onChange={defineCount} />
-      )}
+      {show && <div className={classes.value}>{count}</div>}
       <div>
         <button onClick={inc}>Increment</button>
         <button onClick={inc10}>Increase by 10</button>
         <button onClick={dec}>Decrement</button>
       </div>
-      <button>Toggle Counter</button>
+      <button onClick={toogle}>Toggle Counter</button>
     </main>
   );
 };
--- src/features/counterSlice.js
+++ src/features/counterSlice.js
@@ -1,11 +1,4 @@
-import { useState, useReducer } from "react";
-
-const initialState = {
-  count: 0,
-  step: 1,
-};
-
-const counterReducer = (state = initialState, action) => {
+const counterReducer = (state, action) => {
   switch (action.type) {
     case "dec":
       return { ...state, count: state.count - state.step };
@@ -13,28 +6,25 @@ const counterReducer = (state = initialState, action) => {
       return { ...state, count: state.count + state.step };
     case "inc10":
       return { ...state, count: state.count + 10 };
+    case "toogle":
+      return { ...state, show: !state.show };
     default:
       throw new Error("Unknown action");
   }
 };
 
 export const dec = () => {
-  return { type: "dec" };
+  dispatch({ type: "dec" });
 };
 
 export const inc = () => {
-  return { type: "inc" };
+  dispatch({ type: "inc" });
 };
 export const inc10 = () => {
-  return { type: "inc10" };
+  dispatch({ type: "inc10" });
 };
-
-export const defineCount = (e) => {
-  return { type: "setCount", payload: Number(e.target.value) };
-};
-
-export const defineStep = (e) => {
-  return { type: "setStep", payload: Number(e.target.value) };
+export const toogle = () => {
+  dispatch({ type: "toogle" });
 };
 
 export default counterReducer;
